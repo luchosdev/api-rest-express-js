e1f37cf1af6ad90ce5d5055e1e4f14ebeb433518
Luis Sequera <luissequeracontreras@gmail.com>
2022-09-06 20:30:55 -0500

    [ADD] files

---
+++ routes/products.router.js
@@ -0,0 +1,49 @@
+const express = require('express');
+
+const ProductsService = require('./../services/product.service');
+
+const router = express.Router();
+const service = new ProductsService();
+
+router.get('/', async (req, res) => {
+  const products = await service.find();
+  res.json(products);
+});
+
+router.get('/filter', (req, res) => {
+  res.send('Yo soy un filter');
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const product = await service.findOne(id);
+  res.json(product);
+});
+
+router.post('/', async (req, res) => {
+  const body = req.body;
+  const newProduct = await service.create(body);
+  res.status(201).json(newProduct);
+});
+
+router.patch('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const product = await service.update(id, body);
+    res.json(product);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    });
+  }
+
+});
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const rta = await service.delete(id);
+  res.json(rta);
+});
+
+module.exports = router;
